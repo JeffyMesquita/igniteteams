ce2ae15c41ebb0167536010e99fd581f966b884c
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 17:18:52 -0300

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,6 +4,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
   return (
@@ -19,6 +20,7 @@ export function Players() {
         <Input placeholder="Player Name" autoCorrect={false} />
         <ButtonIcon icon="add" size={30} />
       </Form>
+      <Filter title="Team A" isActive />
     </Container>
   );
 }
