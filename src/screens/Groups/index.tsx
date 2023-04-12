3bbbdf3dbf9fbda7204ddb0a73e1c13b11889843
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 12:58:42 -0300

    Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,20 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
       <Highlight 
         title="Turmas"
         subTitle="Jogue com a sua galera"
+      />
+
+      <GroupCard 
+        title='Os Parças'
       />
     </Container>
   );
