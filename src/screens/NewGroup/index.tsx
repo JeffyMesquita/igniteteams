69752d74c35d47111c9435c2e6b8276fd2a7aa20
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 14:21:06 -0300

    Criando Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from '@components/Header';
 import { Container, Content, Icon } from './styles';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -16,8 +17,13 @@ export function NewGroup() {
           subTitle="Create a group to play with your friends"
         />
 
+        <Input />
+
         <Button 
           title="Create Group"
+          style={{
+            marginTop: 20,
+          }}
         />
       </Content>
     </Container>
