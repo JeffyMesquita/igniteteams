dbc78f81c3d998b97cbae66a01c3829e6336575f
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 11:19:23 -0300

    Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import { Groups } from "@screens/Groups";
+import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components';
+
+import { Groups } from '@screens/Groups';
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
