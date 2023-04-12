9c967e804ee85062e7b0b079868731e130633156
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 13:21:01 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,12 +5,13 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
-    'Os Parças',
-    'Conhecidos do Trabalho',
-    'Amigos da Faculdade',
+    // 'Os Parças',
+    // 'Conhecidos do Trabalho',
+    // 'Amigos da Faculdade',
   ]);
 
   return (
@@ -23,6 +24,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
