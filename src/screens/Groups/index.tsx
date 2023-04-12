62fd317455cd3f3b420c9904aad9268fab9f0e63
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-12 13:11:50 -0300

    Listando gruposÏ

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,28 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Os Parças',
+    'Conhecidos do Trabalho',
+    'Amigos da Faculdade',
+  ]);
+
   return (
     <Container>
       <Header />
 
-      <Highlight 
-        title="Turmas"
-        subTitle="Jogue com a sua galera"
-      />
+      <Highlight title="Turmas" subTitle="Jogue com a sua galera" />
 
-      <GroupCard 
-        title='Os Parças'
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
     </Container>
   );
