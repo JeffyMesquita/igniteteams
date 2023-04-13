011627dbc7c66e23ce4dcf5f836be8fdc0f9d6a8
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-13 17:21:48 -0300

    Implementando o voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Header } from '@components/Header';
 import { Container, Content, Icon } from './styles';
 import { Highlight } from '@components/Highlight';
@@ -5,6 +7,12 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Testing' });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -26,6 +34,7 @@ export function NewGroup() {
           style={{
             marginTop: 20,
           }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
