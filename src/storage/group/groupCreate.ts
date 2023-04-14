afe64eb6ab6b73b182e6175f6c5855e2ef7396c6
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-04-14 10:07:13 -0300

    Criando uma classe de erros personalizada

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppError } from '@utils/AppError';
+
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { groupsGetAll } from './groupsGetAll';
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Group already exists');
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 
